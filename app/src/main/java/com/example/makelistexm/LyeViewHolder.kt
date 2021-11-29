4abed535b0bf2349c4787e5ae9f18ed39284df91
seogiwon <seokw2240@gmail.com>
2021-11-29 22:57:00 +0900

    mkae activity

--- app/src/main/java/com/example/makelistexm/LyeViewHolder.kt
+++ app/src/main/java/com/example/makelistexm/LyeViewHolder.kt
@@ -5,7 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class LyeViewHolder : RecyclerView.ViewHolder() {
+class LyeViewHolder : RecyclerView.ViewHolder(View.inflate(R.layout.item_main)) {
     val lyeText: TextView = itemView.findViewById(R.id.list_text_view)
 
     val lyeImage: ImageView = itemView.findViewById(R.id.list_image_view)
