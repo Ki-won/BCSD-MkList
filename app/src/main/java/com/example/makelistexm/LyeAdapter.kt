05f3106c9b245ab3c0e2a2a8fc76dc564fe1339b
seogiwon <seokw2240@gmail.com>
2021-11-30 00:45:35 +0900

    fix bug

--- app/src/main/java/com/example/makelistexm/LyeAdapter.kt
+++ app/src/main/java/com/example/makelistexm/LyeAdapter.kt
@@ -4,22 +4,21 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
+
 class LyeAdapter : RecyclerView.Adapter<LyeViewHolder>() {
 
     val list = (1..100).toList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LyeViewHolder {
-        return LyeViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_main, parent, false)
-        )
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_main, parent, false)
+        return LyeViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: LyeViewHolder, position: Int) {
-        holder. = list[position].toString()
+        holder.lyeText.text = list[position].toString()
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
-
 }
--- app/src/main/java/com/example/makelistexm/LyeViewHolder.kt
+++ app/src/main/java/com/example/makelistexm/LyeViewHolder.kt
@@ -1,12 +1,14 @@
 package com.example.makelistexm
 
+import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class LyeViewHolder : RecyclerView.ViewHolder(View.inflate(R.layout.item_main)) {
-    val lyeText: TextView = itemView.findViewById(R.id.list_text_view)
-
+open class LyeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var lyeText: TextView = itemView.findViewById(R.id.list_text_view)
     val lyeImage: ImageView = itemView.findViewById(R.id.list_image_view)
+
 }
